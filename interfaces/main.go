1bfed31127d52a44b7626bdd5d9e815d5fba3210
Tibor Kircsi <tkircsi@gmail.com>
2020-09-27 21:36:06 +0200

    interfaces initial setup

---
+++ interfaces/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// Custom logic for generating greeting
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	// Custom logic for generating spanish greeting
+	return "Hola!"
+}
